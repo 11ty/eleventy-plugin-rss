5ab975a1f9a499e182ae58e56cd18ad8bc2fbe15
Zach Leatherman <zachleatherman@gmail.com>
2018-01-28 15:59:39 -0600

    First commit, migrates this code from `eleventy-base-blog`
    v1.0.0

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+const dateToISO = require("./src/dateToISO");
+const absoluteUrl = require("./src/absoluteUrl");
+const htmlToAbsoluteUrls = require("./src/htmlToAbsoluteUrls");
+
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addFilter("rssLastUpdatedDate", collection => {
+    if( !collection || !collection.length ) {
+      throw new Error( "Collection is empty in lastUpdatedDate filter." );
+    }
+
+    // Newest date in the collection
+    return dateToISO(collection[ collection.length - 1 ].date);
+  });
+
+  eleventyConfig.addFilter("rssDate", dateObj => dateToISO(dateObj));
+
+  eleventyConfig.addNunjucksFilter("absoluteUrl", (href, base) => absoluteUrl(href, base));
+
+  eleventyConfig.addNunjucksAsyncFilter("htmlToAbsoluteUrls", (htmlContent, base, callback) => {
+    if(!htmlContent) {
+      callback(null, "");
+      return;
+    }
+
+    htmlToAbsoluteUrls(htmlContent, base).then(result => {
+      callback(null, result.html);
+    });
+  });
+};
---
+++ src/htmlToAbsoluteUrls.js
@@ -0,0 +1,22 @@
+const posthtml = require('posthtml');
+const urls = require('posthtml-urls')
+const absoluteUrl = require("./absoluteUrl");
+
+module.exports = function(htmlContent, base) {
+  let options = {
+    eachURL: function(url, attr, element) {
+      url = url.trim();
+
+      // #anchor in-page
+      if( url.indexOf("#") === 0 ) {
+        return url;
+      }
+
+      return absoluteUrl(url, base);
+    }
+  };
+
+  let modifier = posthtml().use(urls(options));
+
+  return modifier.process(htmlContent);
+};
