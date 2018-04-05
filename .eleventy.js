1bd0c16b603983d721c5a271eb9e4e59b1f844a7
Zach Leatherman <zachleatherman@gmail.com>
2018-04-04 23:56:02 -0500

    adds namespacing support introduced in Eleventy 0.3.1

--- .eleventy.js
+++ .eleventy.js
@@ -2,28 +2,30 @@ const dateToISO = require("./src/dateToISO");
 const absoluteUrl = require("./src/absoluteUrl");
 const htmlToAbsoluteUrls = require("./src/htmlToAbsoluteUrls");
 
-module.exports = function(eleventyConfig) {
-	eleventyConfig.addNunjucksFilter("rssLastUpdatedDate", collection => {
-    if( !collection || !collection.length ) {
-      throw new Error( "Collection is empty in lastUpdatedDate filter." );
-    }
+module.exports = function(eleventyConfig, pluginNamespace) {
+  eleventyConfig.namespace(pluginNamespace, () => {
+  	eleventyConfig.addNunjucksFilter("rssLastUpdatedDate", collection => {
+      if( !collection || !collection.length ) {
+        throw new Error( "Collection is empty in rssLastUpdatedDate filter." );
+      }
 
-    // Newest date in the collection
-    return dateToISO(collection[ collection.length - 1 ].date);
-  });
+      // Newest date in the collection
+      return dateToISO(collection[ collection.length - 1 ].date);
+    });
 
-  eleventyConfig.addNunjucksFilter("rssDate", dateObj => dateToISO(dateObj));
+    eleventyConfig.addNunjucksFilter("rssDate", dateObj => dateToISO(dateObj));
 
-  eleventyConfig.addNunjucksFilter("absoluteUrl", (href, base) => absoluteUrl(href, base));
+    eleventyConfig.addNunjucksFilter("absoluteUrl", (href, base) => absoluteUrl(href, base));
 
-  eleventyConfig.addNunjucksAsyncFilter("htmlToAbsoluteUrls", (htmlContent, base, callback) => {
-    if(!htmlContent) {
-      callback(null, "");
-      return;
-    }
+    eleventyConfig.addNunjucksAsyncFilter("htmlToAbsoluteUrls", (htmlContent, base, callback) => {
+      if(!htmlContent) {
+        callback(null, "");
+        return;
+      }
 
-    htmlToAbsoluteUrls(htmlContent, base).then(result => {
-      callback(null, result.html);
+      htmlToAbsoluteUrls(htmlContent, base).then(result => {
+        callback(null, result.html);
+      });
     });
   });
 };
