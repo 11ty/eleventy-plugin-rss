d18b45ae0633951677567803d8f3f2341abc873f
Zach Leatherman <zachleatherman@gmail.com>
2021-10-07 11:09:23 -0500

    Remove peerDependencies and move to internal plugin compatibility checks. See https://github.com/11ty/eleventy/issues/2001#issuecomment-937934105

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
+const pkg = require("./package.json");
 const dateRfc3339 = require("./src/dateRfc3339");
 const absoluteUrl = require("./src/absoluteUrl");
 const convertHtmlToAbsoluteUrls = require("./src/htmlToAbsoluteUrls");
 const getNewestCollectionItemDate = require("./src/getNewestCollectionItemDate");
 
 module.exports = function(eleventyConfig, options = {}) {
+  try {
+    eleventyConfig.versionCheck(pkg["11ty"].compatibility);
+  } catch(e) {
+    console.log( `WARN: Eleventy Plugin (${pkg.name}) Compatibility: ${e.message}` );
+  }
+
   eleventyConfig.addNunjucksFilter("absoluteUrl", absoluteUrl);
 
   eleventyConfig.addNunjucksAsyncFilter("htmlToAbsoluteUrls", (htmlContent, base, callback) => {
