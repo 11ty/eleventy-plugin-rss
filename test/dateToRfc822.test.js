d0db96c4aa0edc67ed5e03d7fb5d3bd9e209064a
Bruce Thomas <response.write@gmail.com>
2024-05-12 00:50:01 +0100

    Refator pubDate to use Intl Date formatting

--- test/dateToRfc822.test.js
+++ test/dateToRfc822.test.js
@@ -1,8 +1,18 @@
 const test = require("ava");
 const dateRfc822 = require("../src/dateRfc822.js");
 
-test('parses date correctly', (t) => {
-  const date = new Date(2015, 9, 21, 7, 28, 2, 450);
-  const parsedDate = dateRfc822(date);
-  t.is(parsedDate, 'Wed, 21 Oct 2015 07:28:02 +0000');
+// RFC-822 date format
+// <pubDate>Wed, 02 Oct 2002 08:00:00 EST</pubDate>
+
+test('parses dates correctly', (t) => {
+  const d = new Date();
+  const pubDate = dateRfc822(d);
+
+  const rfc822_date = /^([MTWFS][\w]{2},) (\d{2}) ([JFMASOND][a-z]{2}) (\d{4})/;
+  const rfc822_time = /( \d{2}:\d{2}:\d{2} )/;
+  const rfc822_zone = /([A-Z]{2,3})$/;
+
+  t.regex(pubDate, rfc822_date); // Tue, 04 Jun 2024
+  t.regex(pubDate, rfc822_time); // 04:02:01
+  t.regex(pubDate, rfc822_zone); // GMT (depends where test runs)
 });
