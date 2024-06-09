aa118bc3772a2938aa1b3587f59e4d7d05ecfd9a
Zach Leatherman <zachleat@users.noreply.github.com>
2024-06-08 20:30:51 -0500

    Allow writing of multiple feeds at once #47. Fix bug with rss hardcoded collection.

--- src/virtualTemplate.js
+++ src/virtualTemplate.js
@@ -2,9 +2,7 @@ const pkg = require("../package.json");
 const { DeepCopy } = require("@11ty/eleventy-utils");
 const debug = require("debug")("Eleventy:Rss:Feed");
 
-function getFeedContent(options) {
-  let type = options.type;
-
+function getFeedContent(type, { collectionName, limit }) {
   // Note: page.lang comes from the i18n plugin: https://www.11ty.dev/docs/plugins/i18n/#page.lang
 
   if(type === "rss") {
@@ -17,7 +15,7 @@ function getFeedContent(options) {
     <atom:link href="{{ permalink | htmlBaseUrl(metadata.base) }}" rel="self" type="application/rss+xml" />
     <description>{{ metadata.subtitle }}</description>
     <language>{{ metadata.language or page.lang }}</language>
-    {%- for post in collections.posts | reverse %}
+    {%- for post in collections.${collectionName} | reverse | head(${limit}) %}
     {%- set absolutePostUrl = post.url | htmlBaseUrl(metadata.base) %}
     <item>
       <title>{{ post.data.title }}</title>
@@ -40,13 +38,13 @@ function getFeedContent(options) {
   <subtitle>{{ metadata.subtitle }}</subtitle>
   <link href="{{ permalink | htmlBaseUrl(metadata.base) }}" rel="self" />
   <link href="{{ metadata.base | addPathPrefixToFullUrl }}" />
-  <updated>{{ collections['${options.collectionName}'] | getNewestCollectionItemDate | dateToRfc3339 }}</updated>
+  <updated>{{ collections['${collectionName}'] | getNewestCollectionItemDate | dateToRfc3339 }}</updated>
   <id>{{ metadata.base | addPathPrefixToFullUrl }}</id>
   <author>
     <name>{{ metadata.author.name }}</name>
     <email>{{ metadata.author.email }}</email>
   </author>
-  {%- for post in collections['${options.collectionName}'] | reverse | head(${options.limit}) %}
+  {%- for post in collections['${collectionName}'] | reverse | head(${limit}) %}
   {%- set absolutePostUrl %}{{ post.url | htmlBaseUrl(metadata.base) }}{% endset %}
   <entry>
     <title>{{ post.data.title }}</title>
@@ -74,7 +72,7 @@ function getFeedContent(options) {
     }
   ],
   "items": [
-    {%- for post in collections['${options.collectionName}'] | reverse | head(${options.limit}) %}
+    {%- for post in collections['${collectionName}'] | reverse | head(${limit}) %}
     {%- set absolutePostUrl %}{{ post.url | htmlBaseUrl(metadata.base) }}{% endset %}
     {
       "id": "{{ absolutePostUrl }}",
@@ -92,6 +90,10 @@ function getFeedContent(options) {
   throw new Error("Missing or invalid feed type. Received: " + type);
 }
 
+function getInputPath(type) {
+  return `virtual:eleventy-plugin-rss:${type}.njk`;
+}
+
 async function eleventyFeedPlugin(eleventyConfig, options = {}) {
   eleventyConfig.versionCheck(">=3.0.0-alpha.11");
 
@@ -103,13 +105,11 @@ async function eleventyFeedPlugin(eleventyConfig, options = {}) {
   let pluginHtmlBase = await eleventyConfig.resolvePlugin("@11ty/eleventy/html-base-plugin");
   eleventyConfig.addPlugin(pluginHtmlBase, options.htmlBasePluginOptions || {});
 
-  let type = options.type || "atom";
-
   options = DeepCopy({
     collectionName: false, // required
-    type: "atom",
-    outputPath: "/feed.xml",
-    inputPath: `eleventy-plugin-rss_virtual_${type}.njk`,
+    files: {
+      "atom": "/feed.xml"
+    },
     limit: 0, // limit number of entries, 0 means no limit
     metadata: {
       title: "Blog Title",
@@ -127,8 +127,7 @@ async function eleventyFeedPlugin(eleventyConfig, options = {}) {
     throw new Error("Missing `collectionName` option in feedPlugin from @11ty/eleventy-plugin-rss.");
   }
 
-  let feedData = {
-    permalink: options.outputPath,
+  let templateData = {
     eleventyExcludeFromCollections: true,
     eleventyImport: {
       collections: [ options.collectionName ],
@@ -147,7 +146,26 @@ async function eleventyFeedPlugin(eleventyConfig, options = {}) {
     }
   };
 
-  eleventyConfig.addTemplate(options.inputPath, getFeedContent(options), feedData);
+  for(let type in options.files) {
+    let files = options.files[type];
+    let inputPath;
+    let outputPath;
+
+    if(Array.isArray(files) && files.length === 2) {
+      [inputPath, outputPath] = files;
+    } else if(typeof files === "string") {
+      inputPath = getInputPath(type);
+      outputPath = files;
+    } else {
+      throw new Error("Invalid `files` option. Needs an array of length 2 [inputPath, outputPath] or a string `outputPath`");
+    }
+
+    let data = {
+      permalink: outputPath,
+      ...templateData,
+    };
+    eleventyConfig.addTemplate(inputPath, getFeedContent(type, options), data);
+  }
 };
 
 
