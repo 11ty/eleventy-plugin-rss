fca442d6c5b8ca7a50ed232c2b2391b87c81cda0
Zach Leatherman <zachleat@users.noreply.github.com>
2024-06-08 20:10:36 -0500

    Add test for #45

--- test/dateToRfc822.test.js
+++ test/dateToRfc822.test.js
@@ -4,7 +4,13 @@ const dateRfc822 = require("../src/dateRfc822.js");
 // RFC-822 date format
 // <pubDate>Wed, 02 Oct 2002 08:00:00 EST</pubDate>
 
-test('parses dates correctly', (t) => {
+test('parses date correctly (specified date)', (t) => {
+  const date = new Date(2015, 9, 21, 7, 28, 2, 450);
+  const parsedDate = dateRfc822(date);
+  t.true(parsedDate.startsWith('Wed, 21 Oct 2015 07:28:02'));
+});
+
+test('parses dates correctly (open-ended)', (t) => {
   const d = new Date();
   const pubDate = dateRfc822(d);
 
