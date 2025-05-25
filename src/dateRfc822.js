b5dd6a21c8df61665d2a9dc05298adf114cfdea2
Kevin Yank <thatguy@kevinyank.com>
2025-05-25 14:08:47 +1000

    Fix a couple of formatting issues with UTC+10 times
    - Times between midnight and 1AM should begin with "00:", not "24:".
    - When running in a non-GMT time zone, the formatted time should end in a time zone offset (e.g. +1000), not "GMT".

--- src/dateRfc822.js
+++ src/dateRfc822.js
@@ -1,4 +1,4 @@
-module.exports = function pubDateRFC822(value) {
+module.exports = function pubDateRFC822(value, timeZone = undefined) {
   const date = new Date(value);
   const options = {
     weekday: 'short',
@@ -9,14 +9,15 @@ module.exports = function pubDateRFC822(value) {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
-    hour12: false,
+    hourCycle: 'h23',
 
-    timeZoneName: 'short',
+    timeZone: timeZone,
+    timeZoneName: 'longOffset',
   };
 
-  const formatedDate = new Intl.DateTimeFormat('en-US', options).format(date);
-  const [wkd, mmm, dd, yyyy, time, z] = formatedDate.replace(/([,\s+\-]+)/g, ' ').split(' ');
-  const tz = `${z}`.replace(/UTC/, 'GMT');
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+  const [wkd, mmm, dd, yyyy, time, z] = formattedDate.replace(/([,\s]+)/g, ' ').split(' ');
+  const tz = z.replace(/GMT(?<sign>\+|\-)(?<hour>\d\d):(?<minute>\d\d)/, '$<sign>$<hour>$<minute>');
 
   return `${wkd}, ${dd} ${mmm} ${yyyy} ${time} ${tz}`;
 }
